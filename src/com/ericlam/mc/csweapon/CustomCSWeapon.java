3c04eabc4cb9d182f776526d1c948eca5449bdbf
Lam <tsukiko852@gmail.com>
2019-08-01 15:45:21 +0800

    fix crackshot problem

--- src/com/ericlam/mc/csweapon/CustomCSWeapon.java
+++ src/com/ericlam/mc/csweapon/CustomCSWeapon.java
@@ -1,8 +1,11 @@
 package com.ericlam.mc.csweapon;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.utility.MinecraftVersion;
 import com.ericlam.mc.csweapon.api.CCSWeaponAPI;
 import com.ericlam.mc.csweapon.api.MolotovManager;
 import com.hypernite.mc.hnmc.core.main.HyperNiteMC;
+import com.shampaggon.crackshot.MaterialManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +14,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public class CustomCSWeapon extends JavaPlugin implements Listener, CCSWeaponAPI {
     private static CCSWeaponAPI api;
@@ -20,12 +25,29 @@ public class CustomCSWeapon extends JavaPlugin implements Listener, CCSWeaponAPI
         return api;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void onEnable() {
         api = this;
         new ConfigManager(this).loadConfig();
         getServer().getPluginManager().registerEvents(new WeaponListeners(this), this);
         getServer().getPluginManager().registerEvents(new FlashBangListeners(), this);
+
+        // only fix in 1.14
+        if (!ProtocolLibrary.getProtocolManager().getMinecraftVersion().isAtLeast(MinecraftVersion.VILLAGE_UPDATE))
+            return;
+
+        // Fix CrackShot 351~11 problem in 1.14.*
+        try {
+            Class<?> cls = MaterialManager.class;
+            Field materialMapField = cls.getDeclaredField("map");
+            materialMapField.setAccessible(true);
+            Map<String, String> materialMap = (Map<String, String>) materialMapField.get(null);
+            materialMap.replace("351~11", "YELLOW_DYE");
+            this.getLogger().warning("CrackShot doesn't support 1.14.*, but we have fixed the material type problem.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
